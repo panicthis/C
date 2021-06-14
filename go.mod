b1633fb25c73a7be18f2822b7332bb83e7f40855
chaoyuepan <chaoyuepan@baidu.com>
2021-06-14 11:12:00 +0800

    add example

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/panicthis/C
+
+go 1.16
+
+require github.com/panicthis/D v1.1.0
---
+++ lib.go
@@ -0,0 +1,12 @@
+package C
+
+import (
+	"fmt"
+
+	"github.com/panicthis/D"
+)
+
+func Visit() {
+	fmt.Println("C 1.1.0")
+	D.Visit()
+}
